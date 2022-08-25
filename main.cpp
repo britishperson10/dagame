e06f5a761eb5939e8dee1df7133614349663576d
Joseph Rohani <britishperson10@protonmail.com>
2022-08-25 08:03:18 -0700

    Periodic sync

--- main.cpp
+++ main.cpp
@@ -1,18 +1,37 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <sys/ioctl.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
 }
 void processInput(GLFWwindow *window){
-    if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS){
-        glfwSetWindowShouldClose(window, true);
+    if(glfwGetKey(window, GLFW_KEY_Q) a== GLFW_PRESS){
+        glfwSetWindowShouldClose(window, 1);
+		exit(0);
 	}
+}
+int shaderCompSucc(unsigned int vertexShader){
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);	
+	std::cout<<infoLog;
+	if(!success){
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	return success;
 }
 
 int main(){
+	// stupid ass shader ong v
+	const char *vertexShaderSource="#version 330 core
+	layout (location = 0) in vec3 aPos;
+
+	void main()
+	{
+    	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	}"
 	int width, height;
 	width=999; height=999;
     glfwInit();
@@ -20,7 +39,19 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	
+	float vertices[] = { //VBO Vertexes
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+	};  
+	unsigned int VBO;
+	glGenBuffers(1, &VBO); //Generates Vertex Buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //Bind the VBO to a the vertex array
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //Copy the vertex data into memory
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER); //Creating the shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); //Pass shader source code
+	glCompileShader(vertexShader); //Compilethe shader
 	GLFWwindow* window = glfwCreateWindow(width, height, "God help me.", NULL, NULL);
 	if (window == NULL){
 		std::cout << "Failed to create GLFW window" << std::endl;
